a9fe9608b6e2b1ef31fba8c9560fe64fece827ad
Xochitl Medina <medinaxochitl98@gmail.com>
2020-09-17 23:50:00 -0700

    changes to creating the basic cell of timelines and login in and out

---
+++ Twitter/LoginViewController.swift
@@ -0,0 +1,50 @@
+//
+//  LoginViewController.swift
+//  Twitter
+//
+//  Created by Xochitl Medina on 9/17/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        if UserDefaults.standard.bool(forKey: "userLoggedIn") == true{
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }
+    }
+    
+    @IBAction func onLoginButton(_ sender: Any) {
+        print("the login button is working")
+        
+        // in order to have clean code, I'm going to create variables instead
+        let myUrl = "https://api.twitter.com/oauth/request_token"
+        TwitterAPICaller.client?.login(url: myUrl, success: {
+            
+            UserDefaults.standard.set(true, forKey: "userLoggedIn")
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }, failure: { (Error) in
+            print("uUable to login.")
+        })
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
